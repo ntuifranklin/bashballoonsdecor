e32bf95fb2a411942bb81a36bd3eb18e2b18c7b4
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2023-10-08 20:16:31 -0400

    updated mvp. Will add to main branch

--- server.js
+++ server.js
@@ -26,11 +26,18 @@ app.use(express.static(path.join(__dirname, `./${template_folder}`)));
 
 app.locals.siteName = 'Bash Balloons Decor and Rentals';
 app.locals.pageTitle= 'All Your Party Rentals and Decoration Needs';
-app.locals.companyAddress='Hollow Log Dr, Upper Marlboro, MD 20774';
-app.locals.customerServiceNumber= '+1(301)346-1870';
-app.locals.customerServiceNumberExtra = '+1(240)505-3664';
+app.locals.companyAddress='7823 Parston Dr K, Suite E & F, Forestville MD, 20747';
+app.locals.customerServiceNumber= '+1(240)-505-3664';
+app.locals.customerServiceNumberExtra = '+1(240)755-1220';
 app.locals.customerServiceEmail = 'asong_nic@yahoo.com';
-app.locals.companyAddress = 'Hollow Log Dr, Upper Marlboro, MD 20774';
+app.locals.mediumLink = 'https://medium.com/@citation.bashballoonsdecor';
+app.locals.facebookLink = 'https://www.facebook.com/BashBalloonDecorDeliveryInc';
+app.locals.instagramLink = 'https://www.instagram.com/bashballoons_decor/';
+app.locals.twitterLink = 'https://twitter.com/asong_nic';
+app.locals.youtubeLink = 'https://www.youtube.com/@bashballoonsdecor2333';
+app.locals.googleMapsLink ='https://maps.app.goo.gl/6GXT9YBSDnazZXwZ7';
+
+//app.locals.companyAddress = 'Hollow Log Dr, Upper Marlboro, MD 20774';
 //setting global variables to be used by the whole app: 
 app.use((request, response, next) => { 
     return next();
