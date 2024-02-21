0a31ed4994661e75a3a015dc38c700f1c8be6ae0
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2024-02-21 16:03:02 +0000

    made changes

--- routes/rental-items-list.js
+++ routes/rental-items-list.js
@@ -10,10 +10,12 @@ module.exports = () => {
         
     router.get('/', async (request, response) => { 
         
-        var categories = request.locals.categories;
+        var categories = request.session.categories;
+        var category_items = request.session.categoriesItemsHash;
         response.render('layout', { 
             pageTitle: 'Individual Items | Individual Products', 
             template: 'rental-items-list',
+            category_items: category_items,
             list_of_items : request.locals.list_of_items,
             category: 'Individual Party Rental Items',
             csrfToken: request.csrfToken(),
