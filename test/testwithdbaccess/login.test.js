593ee7e9083fa053a07f1d12e552a9da1ef9a634
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2024-04-24 00:18:19 +0000

    added some mocha js testing, needs more refining

--- test/testwithdbaccess/login.test.js
+++ test/testwithdbaccess/login.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../../server.js");
 const { assert,expect,should } = require ("chai");  
+const cheerio = require("cheerio");
 
 const {MySQLDBConnector,defaultMySQLDBConnectorConfig} = require("../../database/models/MySQLDBConnector.js");
 
@@ -13,19 +14,51 @@ describe('GET /login', () => {
   it("Testing the login url",async() => {
       const result = await request(app).get(`/login`) ;
       expect(result.status).to.equal(200);  
-      const html = result.body;
-      const csrf = html._csrf ;
+      const $ = cheerio.load(result.text);
+      const csrf = $("[name=_csrf]") ;
+      //console.log(`obtained csrf: ${csrf}`);
       request(app)
         .post('/login')
         .send({
-          _csrf: csrf,
+          _csrf:csrf,
           email:email,
           password:password
-        }).expect((loginResult)=>{
-          console.log(`${loginResult}`);
-          assert.notEqual(null,loginResult);
+        }).expect(async(loginResult) => {
+          const $ = cheerio.load(loginResult.text);
+          expect($).to.be.an('object');
           expect(loginResult.status).to.equal(200);
-        });
+          expect($("[name=user_email]").val()).to.not.be(null);
+          
+          expect(loginResult.body.data).to.have.property('user_email');
+          expect(loginResult.body.data).to.have.property('otp');
+
+          /* get otp password */
+      
+          const dbConn = MySQLDBConnector;
+          const selectOtpPassword = `select * from otp where user_email= ? 
+          AND expiration_time=(select MAX(expiration_time) from otp where user_email=?)`;
+          var otpData = await dbConn.execute(selectOtpPassword, [email,email]);
+          var otpObject = JSON.parse(JSON.stringify(otpData));
+          const otp = otpObject.otp_code ;
+          /* checking that the verify otp password inserts a generated otp into the otp table */
+          const uemail = otpObject.user_email ;
+          expect(uemail).to.equal(email)
+          const csrf2 = loginResult.body.data._csrf2;
+          request(app)
+            .post('/verifyotp')
+            .send({
+              user_email:email,
+              otp:otp,
+              _csrf:csrf
+            }).expect((verifyOtpResult) => {
+
+              expect(verifyOtpResult.body).to.be.an('object');
+              expect(verifyOtpResult.status).to.equal(200);
+            });
+
+        })
+        
+     
   });
     
 }) ;
