6154434ec0a81d858b293b74713b06afd8f2eea9
Franklin Nkokam Ngongang <ntuifranklin2005@gmail.com>
2024-05-09 21:57:42 +0000

    added domain name blocking

--- utilities/firewall.js
+++ utilities/firewall.js
@@ -1,4 +1,6 @@
 const rejected_domains = ["mmahan.io", "eignh.com"];
+require('dotenv').config();
+const accepted_domains = [new String(process.env.SEO_SITE_LINK)];
 
 function firewall(req, res, next) {
   const domain = (new String(req.headers.host)).toLocaleLowerCase(); 
@@ -13,10 +15,15 @@ function firewall(req, res, next) {
 function isAllowedDomain(domain) {
     for (let k=0; k < rejected_domains.length; k++) {
         if (domain.includes(rejected_domains[k])){
-            //console.log(`firewall, d: ${domain},r: ${rejected_domains[k]}`);
+            console.log(`firewall, d: ${domain},r: ${rejected_domains[k]}`);
             return false ;
-        }
-            
+        }      
+    };
+    for (let k=0; k < accepted_domains.length; k++) {
+      if (!domain.includes(accepted_domains[k])){
+          console.log(`firewall, d: ${domain},r: ${accepted_domains[k]}`);
+          return false ;
+      }      
     }
   return true ;
 } ;
